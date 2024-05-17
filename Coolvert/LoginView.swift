4065320c9feb7c6142b1b42f658984fa6749d032
Alysson <systemskings@gmail.com>
2024-05-17 13:40:25 -0300

    test

--- Coolvert/LoginView.swift
+++ Coolvert/LoginView.swift
@@ -105,9 +105,9 @@ struct LoginView: View {
     }
     
     struct LoginView_Previews: PreviewProvider {
-        static var previews: some View {
-            LoginView()
-        }
+		static var previews: some View {
+			LoginView()
+		}
     }
 }
 
