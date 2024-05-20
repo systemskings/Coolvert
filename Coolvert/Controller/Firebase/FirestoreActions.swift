a67c690efc0f419d9aedef375ae47991d847fd80
Kings Systems <systemskings@gmail.com>
2024-05-20 19:13:18 -0300

    Update FirestoreActions.swift
    alteracao de teste para pull

--- Coolvert/Controller/Firebase/FirestoreActions.swift
+++ Coolvert/Controller/Firebase/FirestoreActions.swift
@@ -6,7 +6,7 @@ class FirestoreActions {
     let firestore = Firestore.firestore()
     
     func cadastrarUsuario( nome: String, ) {
-        let dados: [String : Any] = [ "idade" : 21 , "nome " : "joao" ]
+        let dados: [String : Any] = [ "idade" : 21 , "nome " : "joaotesteteste" ]
         firestore.collection("usuarios").document("jpreis").setData(dados)
     }
     
